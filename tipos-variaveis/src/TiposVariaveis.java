ca089830d093ced820a5246377fc7714cf6438ca
Felicio <victor_felicio@hotmail.com>
2024-08-11 15:22:26 -0300

    feat: oito tipos primitivos do java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,11 @@
 public class TiposVariaveis {
+    char one = 1;
+    byte dia = 1;
+    short ano = 2000;
+    int doc = 4343994;
+    long distancia = 15000000000L;
+    double nota = 7.45445;
+    float media = 7.53f;
+    boolean isLoading = true;
 
 }
